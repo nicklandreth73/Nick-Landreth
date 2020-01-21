0deec310676d8b47fac5e1d11e26efb38e0da2df
nicklandreth73 <60051827+nicklandreth73@users.noreply.github.com>
2020-01-21 00:04:01 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('images/logo').html() + '</span>' +
+					'<span class="title">' + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
